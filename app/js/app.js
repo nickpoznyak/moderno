2202302250fd1354e7d9b7ac98aac96f0c1cd568
nickpoznyak <nickpoznyak72@gmail.com>
2020-07-17 19:40:23 +0300

    settings page

--- app/js/app.js
+++ app/js/app.js
@@ -53,8 +53,16 @@ document.addEventListener("DOMContentLoaded", function() {
 			$(this).addClass('active');
 			$('#'+id).addClass('active-tab').fadeIn();
 			return false;
-		});
+	});
 
+	$('.settings__tabs .tab').on('click', function(event) {
+		var id = $(this).attr('data-id');
+			$('.settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+			$('.settings__tabs .tabs').find('.tab').removeClass('active');
+			$(this).addClass('active');
+			$('#'+id).addClass('active-tab').fadeIn();
+			return false;
+	});
 
 	var containerEl = document.querySelector(".products__inner-box");
 	var mixer;
